97aa8a9dbd672143c14d6f1332bebe3e1d7fc558
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-07 00:53:12 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 
 echo "Hello World!";
 
-DEFINE ('DB_USER','username');
-DEFINE ('DB_PASSWORD','password');
-DEFINE ('DB_HOST','localhost');
-DEFINE ('DB_NAME','finaldb'); 
+//DEFINE ('DB_USER','username');
+//DEFINE ('DB_PASSWORD','password');
+//DEFINE ('DB_HOST','localhost');
+//DEFINE ('DB_NAME','finaldb'); 
 
-$dbc = @ mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+//$dbc = @ mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+$con=mysqli_init(); [mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);] mysqli_real_connect($con, {your_host}, {username@servername}, {your_password}, {your_database}, {your_port});
 	
 
-if(!$dbc) {
+if(!$con) {
 	echo "not connected - " . mysqli_connect_error(); 
 }else {
 	echo 'connected !?';
