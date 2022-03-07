69cd8b3046deb2fb246d0c3a5a7c2a9f0630df89
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-06 22:47:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,16 @@ DEFINE ('DB_PASSWORD','password');
 DEFINE ('DB_HOST','localhost');
 DEFINE ('DB_NAME','finaldb');
 
-$dbc = @ mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-	or die ('Could not connect to mysql: ' . mysqli_connect_error() );
+$dbc = @ mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	
+
+if(!$dbc) {
+	echo "not connected - " . mysqli_connect_error(); 
+}else {
+	echo 'connected !?'
+	
+}
+	
 
 ?>
  
