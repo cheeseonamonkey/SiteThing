4bc7daacc5810b966bfd1c0cde0ace18865adbf4
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-07 04:20:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,8 @@ $connectionInfo = array("UID" => "ffatty", "pwd" => "password12345$", "Database"
 $serverName = "tcp:site-thing-sql-server.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
     
+	echo '<br>errors:<br>'
+    echo mysqli_error();
     echo '<br>connection info:<br>';
     echo var_dump($connectionInfo);
     echo '<br>server name:<br>';
