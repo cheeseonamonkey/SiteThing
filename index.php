6bcc0070a8f8ae62973ae01862700f8ec6287619
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-06 22:37:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 echo "Hello World!";
 
+DEFINE ('DB_USER','username');
+DEFINE ('DB_PASSWORD','password');
+DEFINE ('DB_HOST','localhost');
+DEFINE ('DB_NAME','finaldb');
 
 $dbc = @ mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 	or die ('Could not connect to mysql: ' . mysqli_connect_error() );
