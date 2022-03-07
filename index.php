5c0f04df28d79346e99b66c7c3ad68a1cd8c0b0c
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-07 01:57:57 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,16 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:site-thing-sql-server.database.windows.net,1433; Database = SiteThingDB", "ffatty", "password12345$");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "ffatty", "pwd" => "password12345$", "Database" => "SiteThingDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:site-thing-sql-server.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+    
+    echo $connectionInfo;
+    echo $serverName;
+    
+    
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
@@ -12,10 +22,7 @@ catch (PDOException $e) {
 
 
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ffatty", "pwd" => "password12345$", "Database" => "SiteThingDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:site-thing-sql-server.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
 
 
