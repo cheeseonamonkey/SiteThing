261b00f2847ee044f4c45ec37f8c4463ee8ecc4e
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-09 19:03:04 -0700

    Update index.php

--- index.php
+++ index.php
@@ -33,11 +33,21 @@ try {
 //	
 //	
 
+	echo "path:<br>";
+	echo getcwd();
 	
+	echo "files:<br>";
+	$files = scandir(getcwd());
+	$files = array_diff(scandir($path), array('.', '..'));
+	foreach($files as $file){
+  		echo "<a href='$file'>$file</a>";
+	}
+	
+	echo "<br>connecting...";
 	$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL); mysqli_real_connect($conn, "sitething-server.mysql.database.azure.com", "vcyswvxapq", "FXDUBP1OQ8K8D2X6$", "stdb", 3306, MYSQLI_CLIENT_SSL);
 	
 	
-	echo "connection errors:<br>" . var_dump(mysqli_error());
+	echo "<br>connection errors:<br>" . var_dump(mysqli_error());
 	
 	
 		
