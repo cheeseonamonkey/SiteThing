3203d446141b75c0992c6612234b181491c3f576
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-09 22:31:37 -0700

    Update sqlConnect.php

--- includes/sqlConnect.php
+++ includes/sqlConnect.php
@@ -12,14 +12,14 @@ $serverName = "sqlserverass.database.windows.net"; // update me
          FROM [SalesLT].[ProductCategory] pc
          JOIN [SalesLT].[Product] p
          ON pc.productcategoryid = p.productcategoryid";
-    $getResults= sqlsrv_query($conn, $tsql);
+    $results= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
+    if ($results == FALSE)
         echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    while ($row = sqlsrv_fetch_array($results, SQLSRV_FETCH_ASSOC)) {
      echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
     }
-    sqlsrv_free_stmt($getResults);
+    sqlsrv_free_stmt($results);
 
 
 
