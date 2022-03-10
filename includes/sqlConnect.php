b11c87f4829e65ecda69bb3e512be4766e4c7294
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-09 21:54:53 -0700

    Create sqlConnect.php

---
+++ includes/sqlConnect.php
@@ -0,0 +1,26 @@
+<?php
+
+$serverName = "sqlserverass.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "stdb", // update me
+        "Uid" => "banana", // update me
+        "PWD" => "asdf123$" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+         FROM [SalesLT].[ProductCategory] pc
+         JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+
+
+
+?>
