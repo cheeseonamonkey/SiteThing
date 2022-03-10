73372d849901dde3d13696b5c4a36cda0846f761
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-03-10 14:38:48 -0700

    Update sqlConnect.php

--- includes/sqlConnect.php
+++ includes/sqlConnect.php
@@ -1,5 +1,7 @@
 <?php
 
+try
+{
 $serverName = "sqlserverass.database.windows.net"; // update me
     $connectionOptions = array(
         "Database" => "stdb", // update me
@@ -7,20 +9,26 @@ $serverName = "sqlserverass.database.windows.net"; // update me
         "PWD" => "asdf123$" // update me
     );
     //Establishes the connection
-    $dbc = sqlsrv_connect($serverName, $connectionOptions);
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
     $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
          FROM [SalesLT].[ProductCategory] pc
          JOIN [SalesLT].[Product] p
          ON pc.productcategoryid = p.productcategoryid";
-    $results= sqlsrv_query($dbc, $tsql);
+    $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
-    if ($results == FALSE)
+    if ($getResults == FALSE)
         echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($results, SQLSRV_FETCH_ASSOC)) {
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
      echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
     }
-    sqlsrv_free_stmt($results);
 
+    sqlsrv_free_stmt($getResults);
 
+}catch(Exception $e)
+{
+        echo "Error - " . $e; 
+    
+    die(print_r($e));
+}
 
 ?>
